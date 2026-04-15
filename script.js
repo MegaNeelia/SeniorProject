7f0736b3f4c7af086c99d830e9018d4e69fa4b3c
MegaNeelia <aileenmega011@gmail.com>
2026-04-14 17:24:16 -0700

    finished adding the button and other stuff i lowk forgor

--- script.js
+++ script.js
@@ -2,13 +2,33 @@ const dropArea = document.getElementById("drop-area");
 const inputFile = document.getElementById("input-file");
 const imageView = document.getElementById("img-view");
 
+let allFiles = [];
+
 inputFile.addEventListener("change", uploadImage);
 
 function uploadImage() {
+    const files = inputFile.files;
+    const gallery = document.getElementById("thumbnail-gallery");
+
+    for (const file of files) {
+        allFiles.push(file);
+        let imgLink = URL.createObjectURL(inputFile.files[0]);
+        imageView.style.backgroundImage = `url(${imgLink})`;
+        imageView.textContent = "";
+        imageView.style.border = 0;
+
+        let thumb = document.createElement("img");
+        thumb.src = imgLink;
+        thumb.classList.add("thumb");
+        gallery.appendChild(thumb);
+    }
+
+    /*
     let imgLink = URL.createObjectURL(inputFile.files[0]);
     imageView.style.backgroundImage = `url(${imgLink})`;
     imageView.textContent = "";
     imageView.style.border = 0;
+    */
 }
 
 dropArea.addEventListener("dragover", function(e){
@@ -19,4 +39,13 @@ dropArea.addEventListener("drop", function(e){
     e.preventDefault();
     inputFile.files = e.dataTransfer.files;
     uploadImage();
+});
+
+document.getElementById("submit-btn").addEventListener("click", () => {
+    if (allFiles.length === 0) {
+        alert("Please add some images first!");
+        return;
+    }
+    console.log("Uploading these files: ", allFiles);
+    alert("Upload Successful!");
 });
